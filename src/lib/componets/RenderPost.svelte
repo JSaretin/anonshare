2f522dc58c565eb4435e7da205332eeb96288f11
v0idhack <johnosaretin7878@gmail.com>
2022-07-11 04:32:52 +0100

    view post render, haven't work on it

---
+++ src/lib/componets/RenderPost.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Post } from '../structure';
+
+	export let post: Post;
+</script>
+
+<!-- {post.views} -->
+<div class=" border-t-[1px] border-b-[1px] p-8">
+	<p class="py-4">
+		{post.post}
+	</p>
+
+	<div class="flex gap-4 border-t-2 py-4 justify-between">
+		<div class="flex gap-6">
+			<button class="">
+				{post.views} views
+			</button>
+
+			<button>like</button>
+		</div>
+
+		<span>
+			{new Date(post.created_at).toLocaleString()}
+		</span>
+	</div>
+</div>
